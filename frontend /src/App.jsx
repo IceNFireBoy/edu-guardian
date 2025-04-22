a68cd290e2f782fcf1b70dbb2690c7cb33bb10a8
IceNFireBoy <marxusantonio.magisa@gmail.com>
2025-04-22 19:06:17 +0800

    Fixed frontend entry files for Netlify build

---
+++ frontend /src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="min-h-screen flex items-center justify-center text-center text-2xl">
+      Welcome to EduGuardian 🎓✨
+    </div>
+  );
+}
+
+export default App;
