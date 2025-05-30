c704e20b90cfadba0530ac04652e2228b1aa9bc9
IceNFireBoy <marxusantonio.magisa@gmail.com>
2025-05-30 21:59:47 +0800

    fixed prioritization router

--- backend/src/routes/userRoutes.ts
+++ backend/src/routes/userRoutes.ts
@@ -11,13 +11,13 @@ const userController = new UserController();
 // Public routes
 router.get('/leaderboard', UserController.getLeaderboard);
 router.get('/:username/profile', UserController.getUserPublicProfile);
+router.get('/me/badges', protect, UserController.getMyBadges);
 router.get('/:userId/badges', UserController.getUserBadgesById);
 
 // Protected routes
 router.get('/me', protect, UserController.getMe);
 router.put('/me', protect, UserController.updateDetails);
 router.put('/me/password', protect, UserController.updatePassword);
-router.get('/me/badges', protect, UserController.getMyBadges);
 router.get('/me/activity', protect, UserController.getMyActivityLog);
 router.get('/me/notes', protect, UserController.getMyUploadedNotes);
 router.get('/me/favorites', protect, UserController.getMyFavoriteNotes);
